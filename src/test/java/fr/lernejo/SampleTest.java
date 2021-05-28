9c414f126e365c4f4e765027da21e6ad5f01bd98
Mohamad Boukrit <mohamad.boukrit@epita.fr>
2021-05-28 18:01:49 +0200

    Add test to match 100% coverage

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,21 +1,42 @@
 package fr.lernejo;
-import fr.lernejo.Sample;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class SampleTest {
-    private Sample sample;
-    @BeforeEach
-    public void setUp() throws Exception{
-        sample = new Sample();
+    static final Sample s = new Sample();
+
+    @org.junit.jupiter.api.Test
+    void Add_simple() {
+        assertEquals(2, s.op(Sample.Operation.ADD, 1,1));
     }
-    @Test
-    void operation_add_should_succeed() {
-        int a = 7;
-        int b = 3;
-        int result;
-        result = sample.op(Sample.Operation.ADD, a,b);
-        Assertions.assertThat(result).as("check if 7+3 equal 10")
-            .isEqualTo(10);
+
+    @org.junit.jupiter.api.Test
+    void Add_negative() {
+        assertEquals(-10, s.op(Sample.Operation.ADD, 10,-20));
+    }
+
+    @org.junit.jupiter.api.Test
+    void Mult_simple() {
+        assertEquals(2, s.op(Sample.Operation.MULT, 1,2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void Mult_simple2() {
+        assertEquals(100, s.op(Sample.Operation.MULT, 10,10));
+    }
+
+    @org.junit.jupiter.api.Test
+    void Fact_zero() {
+        assertEquals(1, new Sample().fact(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void Fact_ten() {
+        assertEquals(3628800, new Sample().fact(10));
+    }
+
+    @org.junit.jupiter.api.Test
+    void Fact_negative() {
+        assertThrows(IllegalArgumentException.class, () -> new Sample().fact(-10));
     }
 }
