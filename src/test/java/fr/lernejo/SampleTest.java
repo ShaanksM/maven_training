81575d3a273fe5602881895753b728ef33a8bc66
Mohamad Boukrit <mohamad.boukrit@epita.fr>
2021-05-28 17:57:09 +0200

    Add test to match 100% coverage

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,22 +1,21 @@
 package fr.lernejo;
-
+import fr.lernejo.Sample;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 class SampleTest {
-    @Test
-    void add_behaves_as_expected(){
-        int x = 4;
-        int y = 3;
-        int result = new Sample().op(Sample.Operation.MULT,x,y);
-        Assertions.assertThat(result).as("multiple of 4*3").isEqualTo(12);
+    private Sample sample;
+    @BeforeEach
+    public void setUp() throws Exception{
+        sample = new Sample();
     }
     @Test
-    void addition_ok(){
-        int x = 10;
-        int y = 3;
-        int result = new Sample().op(Sample.Operation.ADD,x,y);
-        Assertions.assertThat(result).as("addition of 10 + 3").isEqualTo(13);
-
+    void operation_add_should_succeed() {
+        int a = 7;
+        int b = 3;
+        int result;
+        result = sample.op(Sample.Operation.ADD, a,b);
+        Assertions.assertThat(result).as("check if 7+3 equal 10")
+            .isEqualTo(10);
     }
 }
